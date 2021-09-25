1ca3abcc8fe5a606373e4af539c844ac40c1de89
tgaram <tgaram@gmail.com>
2021-09-25 09:52:48 +0200

    Add server routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const API_BASE_URL = 'https://api.nasa.gov'
 const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
@@ -17,9 +18,32 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // example API call
 app.get('/apod', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        let image = await fetch(`${API_BASE_URL}/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ image })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+app.get('/rovers', async (req, res) => {
+    try {
+        let rovers = await fetch(`${API_BASE_URL}/mars-photos/api/v1/rovers?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send( rovers )
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+app.get('/rovers/:rover/photos/:max_date', async (req, res) => {
+    const roverSlug = req.params.rover;
+    const roverMaxDate = req.params.max_date;
+
+    try {
+        let rover = await fetch(`${API_BASE_URL}/mars-photos/api/v1/rovers/${roverSlug}/photos?earth_date=${roverMaxDate}&page=1&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send( rover )
     } catch (err) {
         console.log('error:', err);
     }
